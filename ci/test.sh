06c820ce8420f3aa347c0fac1472bb3dc2a5e1ed
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-19 19:39:37 +0200

    Collect names of failed packages and echo this list at end of test run.
    Instead of immediately exiting upon failed tests, we run through all the test suites independently of whether the previous one failed, and store the names of the packages that had failed tests.
    
    The list of failing packages is echoed at the end of the test run for convenience.

--- ci/test.sh
+++ ci/test.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-set -x
+set -ex
 
-# Run the functional tests for the framework
+FAILED_PACKAGES=""
+
+# Run the functional tests for the framework.
 BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/wp-cli/wp-cli/features php ci/behat-tags.php)
+
+set +e
 vendor/bin/behat --format progress $BEHAT_TAGS --strict
+if [ $? -ne 0 ]; then
+	FAILED_PACKAGES="$FAILED_PACKAGES wp-cli/wp-cli"
+fi
+set -e
 
 # Run the functional tests for the bundled commands
 REPOS=$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")
+
 for REPO in $REPOS; do
+
 	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$REPO/features php ci/behat-tags.php)
+
+	set +e
 	vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$REPO
+	if [ $? -ne 0 ]; then
+		FAILED_PACKAGES="$FAILED_PACKAGES $REPO"
+	fi
+	set -e
+
 done
+
+if [ $FAILED_PACKAGES -ne "" ]; then
+	echo "Packages with failed tests:$FAILED_PACKAGES"
+	exit 1
+fi
+
+echo "All Packages were successfully tested."
+exit 0
