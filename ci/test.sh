f6735f6edb25bef5ee0960b901264d900353cb3c
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-22 13:03:51 +0200

    Split Travis tests into two instances to get around timeout limit.
    Travis times out at 50 minutes, which we now easily hit if we run all the framework tests as well as all the command tests consecutively in one build.
    
    To get around this timeout, Travis can now provide a variable `"$TEST_PACKAGE"` that denotes what package to test, so we can split this up through the matrix feature.
    
    Options for the `"$TEST_PACKAGE"` variable.
    
    - `"none"`: No packages are tested.
    - `"all"`: The framework as well as all bundled commands are tested.
    - `"commands"`: Only the command packages are tested.
    - `<package name>`: Only the package named `<package name>` is tested.

--- ci/test.sh
+++ ci/test.sh
@@ -1,34 +1,72 @@
 #!/bin/bash
 
+# The tests to be run are selected based on the $TEST_PACKAGE environment
+# variable.
+#
+# The following options can be set:
+#
+# - "none": No packages are tested.
+# - "all": The framework as well as all bundled commands are tested.
+# - "commands": Only the command packages are tested.
+# - <package name>: Only the package named <package name> is tested.
+
 set -ex
 
 FAILED_PACKAGES=""
 
-# Run the functional tests for the framework.
-BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/wp-cli/wp-cli/features php ci/behat-tags.php)
+if [ "$TEST_PACKAGE" -eq "all" -o "$TEST_PACKAGE" -eq "wp-cli/wp-cli" ]; then
 
-set +e
-vendor/bin/behat --format progress $BEHAT_TAGS --strict
-if [ $? -ne 0 ]; then
-	FAILED_PACKAGES="$FAILED_PACKAGES wp-cli/wp-cli"
-fi
-set -e
+	# Run the functional tests for the framework.
+	echo "Testing wp-cli/wp-cli framework."
 
-# Run the functional tests for the bundled commands
-REPOS=$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")
-
-for REPO in $REPOS; do
-
-	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$REPO/features php ci/behat-tags.php)
+	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/wp-cli/wp-cli/features php ci/behat-tags.php)
 
 	set +e
-	vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$REPO
+	vendor/bin/behat --format progress $BEHAT_TAGS --strict
 	if [ $? -ne 0 ]; then
-		FAILED_PACKAGES="$FAILED_PACKAGES $REPO"
+		FAILED_PACKAGES="$FAILED_PACKAGES wp-cli/wp-cli"
 	fi
 	set -e
 
-done
+fi
+
+if [ "$TEST_PACKAGE" -eq "all" -o "$TEST_PACKAGE" -eq "commands" ]; then
+
+	# Run the functional tests for the bundled commands.
+	echo "Testing command packages."
+
+	REPOS=$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")
+
+	for REPO in $REPOS; do
+
+		BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$REPO/features php ci/behat-tags.php)
+
+		set +e
+		vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$REPO
+		if [ $? -ne 0 ]; then
+			FAILED_PACKAGES="$FAILED_PACKAGES $REPO"
+		fi
+		set -e
+
+	done
+
+fi
+
+if [ "$TEST_PACKAGE" -ne "none" -o "$TEST_PACKAGE" -ne "all" -o "$TEST_PACKAGE" -ne "commands" ]; then
+
+	# Run the functional tests for a specific package.
+	echo "Testing package $TEST_PACKAGE."
+
+	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$TEST_PACKAGE/features php ci/behat-tags.php)
+
+	set +e
+	vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$TEST_PACKAGE
+	if [ $? -ne 0 ]; then
+		FAILED_PACKAGES="$FAILED_PACKAGES $TEST_PACKAGE"
+	fi
+	set -e
+
+fi
 
 if [ -n "$FAILED_PACKAGES" ]; then
 	echo "Packages with failed tests:$FAILED_PACKAGES"
