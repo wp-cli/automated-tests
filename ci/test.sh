8326eab4258d1e9d114482d04fac83c16d4414cc
Alain Schlesser <alain.schlesser@gmail.com>
2017-05-22 14:02:34 +0200

    Rewrite test script to provide better control over packages to test.

--- ci/test.sh
+++ ci/test.sh
@@ -14,59 +14,45 @@ set -ex
 
 FAILED_PACKAGES=""
 
-if [ "$TEST_PACKAGE" == "all" -o "$TEST_PACKAGE" == "wp-cli/wp-cli" ]; then
+REPOS=""
 
-	# Run the functional tests for the framework.
-	echo "Testing wp-cli/wp-cli framework."
+if [ -z "$TEST_PACKAGE" -o "$TEST_PACKAGE" == "none" ]; then
+	echo "Skipping feature tests completely."
+	exit 0
+fi
 
-	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/wp-cli/wp-cli/features php ci/behat-tags.php)
+if [ "$TEST_PACKAGE" == "all" ]; then
+	REPOS="wp-cli/wp-cli
+$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")"
+fi
+
+if [ "$TEST_PACKAGE" == "commands" ]; then
+	REPOS="$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")"
+fi
+
+if [ "$TEST_PACKAGE" != "all" -a "$TEST_PACKAGE" != "commands" ]; then
+	REPOS="$TEST_PACKAGE"
+fi
+
+for REPO in $REPOS; do
+
+	echo "Testing package $REPO..."
+
+	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$REPO/features php ci/behat-tags.php)
+
+	BEHAT_PROFILE=""
+	if [ "$REPO" != "wp-cli/wp-cli" ]; then
+		BEHAT_PROFILE=" -p=$REPO"
+	fi
 
 	set +e
-	vendor/bin/behat --format progress $BEHAT_TAGS --strict
+	vendor/bin/behat --format progress $BEHAT_TAGS --strict $BEHAT_PROFILE
 	if [ $? -ne 0 ]; then
-		FAILED_PACKAGES="$FAILED_PACKAGES wp-cli/wp-cli"
+		FAILED_PACKAGES="$FAILED_PACKAGES $REPO"
 	fi
 	set -e
 
-fi
-
-if [ "$TEST_PACKAGE" == "all" -o "$TEST_PACKAGE" == "commands" ]; then
-
-	# Run the functional tests for the bundled commands.
-	echo "Testing command packages."
-
-	REPOS=$(cat vendor/wp-cli/wp-cli/composer.json | grep -oE "wp-cli/([a-z\-]*)-command")
-
-	for REPO in $REPOS; do
-
-		BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$REPO/features php ci/behat-tags.php)
-
-		set +e
-		vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$REPO
-		if [ $? -ne 0 ]; then
-			FAILED_PACKAGES="$FAILED_PACKAGES $REPO"
-		fi
-		set -e
-
-	done
-
-fi
-
-if [ "$TEST_PACKAGE" != "none" -o "$TEST_PACKAGE" != "all" -o "$TEST_PACKAGE" != "commands" ]; then
-
-	# Run the functional tests for a specific package.
-	echo "Testing package $TEST_PACKAGE."
-
-	BEHAT_TAGS=$(BEHAT_FEATURES_FOLDER=vendor/$TEST_PACKAGE/features php ci/behat-tags.php)
-
-	set +e
-	vendor/bin/behat --format progress $BEHAT_TAGS --strict -p=$TEST_PACKAGE
-	if [ $? -ne 0 ]; then
-		FAILED_PACKAGES="$FAILED_PACKAGES $TEST_PACKAGE"
-	fi
-	set -e
-
-fi
+done
 
 if [ -n "$FAILED_PACKAGES" ]; then
 	echo "Packages with failed tests:$FAILED_PACKAGES"
